5a076dbc0002a115c9a9d26a70f149821a4c479c
Meritxell Hernandez morraja <mehernan@cbr15s4.42barcelona.com>
2023-05-22 18:13:50 +0200

    client sending signals done

--- client.c
+++ client.c
@@ -6,14 +6,45 @@
 /*   By: mehernan <mehernan@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 19:55:26 by mehernan          #+#    #+#             */
-/*   Updated: 2023/05/19 19:34:05 by mehernan         ###   ########.fr       */
+/*   Updated: 2023/05/22 18:10:54 by mehernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // this file is just a test to check if my makefile is compilen two files at the same time
 #include "ft_printf.h"
 #include "libft.h"
+#include <signal.h>
+
 // is needed to pas the strig with the fuction kill instead of calling the fuctuin
+int	send_str(char *str, int pid)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while(str[i] != '\0')
+	{
+		while(j < 8)
+		{
+			if((str[i] & (1 << j)) == 1)
+			{
+				printf("its a 1\n");
+				if (kill(pid, SIGUSR2) == -1)
+					return(1);
+			}
+			else
+			{
+				printf("ist a 0\n");
+				if (kill(pid, SIGUSR1) == -1)
+					return(1);
+			}
+			j++;
+		}
+		i++;
+	}
+	return(0);
+}
 int	main(int argc, char *argv[])
 {
 	int	pid;
@@ -26,7 +57,8 @@ int	main(int argc, char *argv[])
 	}
 	pid = ft_atoi(argv[1]);// I send the pid to converter to an int 
 	str = argv[2];
-//	print_str(str);//no funciona, hay que pasarlo con la funcion kill
+	if (send_str(str, pid) == 1)
+		printf("ERROR\n");
 	printf("client is being printed\n");
 //	return(&argv);
 }
--- server.c
+++ server.c
@@ -6,24 +6,25 @@
 /*   By: mehernan <mehernan@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 19:57:33 by mehernan          #+#    #+#             */
-/*   Updated: 2023/05/19 19:34:05 by mehernan         ###   ########.fr       */
+/*   Updated: 2023/05/22 18:10:54 by mehernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //File to test if my makefile can compile two programs at the same time
 #include <unistd.h>
 #include "ft_printf.h"
-int	print_str(char *str)
-{
-	printf("%s\n", str);
-}
+
+//int	print_str(char *str)
+//{
+//	printf("%s\n", str);
+//}
 int	main(void)
 {
 	int server_pid;
 
 	server_pid = getpid();
 	printf("%d\n", server_pid);
-//	client(server_pid, str);
+//	printf("
 	while (1)
 		;
 	printf("server is being printed\n");
