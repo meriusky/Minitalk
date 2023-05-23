0fd53bbb223f1a76b6bb166d7b7c12a1307f408c
Meritxell Hernandez morraja <mehernan@cbr6s5.42barcelona.com>
2023-05-23 18:35:50 +0200

    done without checking errors (printing the string)

--- client.c
+++ client.c
@@ -6,59 +6,55 @@
 /*   By: mehernan <mehernan@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 19:55:26 by mehernan          #+#    #+#             */
-/*   Updated: 2023/05/22 18:10:54 by mehernan         ###   ########.fr       */
+/*   Updated: 2023/05/23 18:31:55 by mehernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// this file is just a test to check if my makefile is compilen two files at the same time
 #include "ft_printf.h"
 #include "libft.h"
 #include <signal.h>
-
-// is needed to pas the strig with the fuction kill instead of calling the fuctuin
 int	send_str(char *str, int pid)
 {
 	int	i;
 	int	j;
 
 	i = 0;
-	j = 0;
 	while(str[i] != '\0')
 	{
+		j = 0;
 		while(j < 8)
 		{
-			if((str[i] & (1 << j)) == 1)
+			if((str[i] & (1 << j)) == 0)
 			{
-				printf("its a 1\n");
-				if (kill(pid, SIGUSR2) == -1)
+				if (kill(pid, SIGUSR1) == -1)
 					return(1);
 			}
 			else
 			{
-				printf("ist a 0\n");
-				if (kill(pid, SIGUSR1) == -1)
+				if (kill(pid, SIGUSR2) == -1)
 					return(1);
 			}
 			j++;
+			usleep(300);
 		}
 		i++;
 	}
 	return(0);
 }
+
 int	main(int argc, char *argv[])
 {
-	int	pid;
+	int		pid;
 	char	*str;
 
 	if(argc != 3)
 	{
-		printf("Error, must have three arguments\n");
+		ft_printf("Error, must have three arguments\n");
 		exit(0);
 	}
-	pid = ft_atoi(argv[1]);// I send the pid to converter to an int 
+	pid = ft_atoi(argv[1]);
 	str = argv[2];
 	if (send_str(str, pid) == 1)
-		printf("ERROR\n");
-	printf("client is being printed\n");
-//	return(&argv);
+		ft_printf("ERROR\n");
+	return (0);
 }
--- server.c
+++ server.c
@@ -6,26 +6,37 @@
 /*   By: mehernan <mehernan@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 19:57:33 by mehernan          #+#    #+#             */
-/*   Updated: 2023/05/22 18:10:54 by mehernan         ###   ########.fr       */
+/*   Updated: 2023/05/23 18:31:55 by mehernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//File to test if my makefile can compile two programs at the same time
 #include <unistd.h>
 #include "ft_printf.h"
 
-//int	print_str(char *str)
-//{
-//	printf("%s\n", str);
-//}
+void	print_string(int sign)
+{
+	static int	count = 0;
+	static char	c = '\0';
+
+	if (sign == SIGUSR2)
+		c = c | (1 << count);
+	count++;
+	if (count > 7)
+	{
+		ft_printf("%c", c);
+		count = 0;
+		c = '\0';
+	}
+}
+
 int	main(void)
 {
 	int server_pid;
 
 	server_pid = getpid();
-	printf("%d\n", server_pid);
-//	printf("
+	ft_printf("%d\n", server_pid);
+	signal(SIGUSR1, &print_string);
+	signal(SIGUSR2, &print_string);
 	while (1)
 		;
-	printf("server is being printed\n");
 }
